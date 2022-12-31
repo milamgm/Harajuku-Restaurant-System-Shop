24007451f19e1b6dbf9b4cf533aae35ec09e81eb
milamgm <nocturnetheater@gmail.com>
2022-12-31 15:56:17 +0100

    .env modified

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -16,12 +16,3 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export default db;
-
-/*
- apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID
-*/
